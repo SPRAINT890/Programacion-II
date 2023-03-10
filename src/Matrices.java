66149c6c37636f17ac575588c487ea604efd1a00
Gaspar Morales <gasparmorales1@gmail.com>
2023-03-10 16:57:28 -0300

    Calcular Matrices

---
+++ src/Matrices.java
@@ -0,0 +1,22 @@
+public class Matrices {
+    /*public void desplegar(int[][] matriz){
+
+    }
+    public boolean esCuadrada(int[][] matriz){
+
+    }
+    public int filas(int[][] matriz){
+
+    }
+    public int columnas(int[][] matriz){
+
+    }
+    public int[][] sumar (int[][] matrizA, int[][] matrizB){
+
+    }*/
+    public static void main(String[] args) {
+        int[][] matriz = new int[2][3];
+        System.out.println(matriz[0].length);
+        System.out.println(matriz.length);
+    }
+}
