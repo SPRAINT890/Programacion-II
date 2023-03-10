45df144281bfd1ed8925a218cebaa01244a246a5
Gaspar Morales <gasparmorales1@gmail.com>
2023-03-10 19:53:04 -0300

    desplegar hecho, esCuadrada a probar

--- src/Matrices.java
+++ src/Matrices.java
@@ -1,15 +1,21 @@
 public class Matrices {
-    public void desplegar(int[][] matriz){
+    public static void desplegar(int[][] matriz){
         for (int y = 0; y < matriz.length; y++){
             for (int x = 0; x < matriz[y].length; x++){
-                System.out.print(matriz[y][x]);
+                System.out.print(matriz[y][x] + " | ");
             }
+            System.out.println("");
         }
     }
-    /*
-    public boolean esCuadrada(int[][] matriz){
 
+    public static boolean esCuadrada(int[][] matriz){
+        boolean resultado = false;
+        if (matriz.length > 0 && matriz[0].length == matriz.length){
+            resultado = true;
+        }
+        return resultado;
     }
+    /*
     public int filas(int[][] matriz){
 
     }
@@ -20,15 +26,19 @@ public class Matrices {
 
     }*/
     public static void main(String[] args) {
-        int[][] matriz = new int[2][3];
-        matriz [0][0] = 1;
-        matriz [0][1] = 7;
-        matriz [0][2] = 4;
-        matriz [1][0] = 1;
-        matriz [1][1] = 3;
-        matriz [1][2] = 8;
-
-        System.out.println(matriz[0].length);
-        System.out.println(matriz.length);
+        int[][] matrizA = new int[2][3];
+        matrizA [0][0] = 1;
+        matrizA [0][1] = 7;
+        matrizA [0][2] = 4;
+        matrizA [1][0] = 1;
+        matrizA [1][1] = 3;
+        matrizA [1][2] = 8;
+        desplegar(matrizA);
+        System.out.println("Largos");
+        System.out.println(matrizA[0].length);
+        System.out.println(matrizA.length);
+        int[][] matrizB = new int[0][0];
+        System.out.println("es cuadrada?: " + esCuadrada(matrizB));
+        System.out.println(matrizB.length);
     }
 }
