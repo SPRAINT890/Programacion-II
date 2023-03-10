63f95b5ec2417a1811effe49699c28a12f44a665
Gaspar Morales <gasparmorales1@gmail.com>
2023-03-10 18:08:56 -0300

    llenado de datos

--- src/Matrices.java
+++ src/Matrices.java
@@ -1,7 +1,12 @@
 public class Matrices {
-    /*public void desplegar(int[][] matriz){
-
+    public void desplegar(int[][] matriz){
+        for (int y = 0; y < matriz.length; y++){
+            for (int x = 0; x < matriz[y].length; x++){
+                System.out.print(matriz[y][x]);
+            }
+        }
     }
+    /*
     public boolean esCuadrada(int[][] matriz){
 
     }
@@ -16,6 +21,13 @@ public class Matrices {
     }*/
     public static void main(String[] args) {
         int[][] matriz = new int[2][3];
+        matriz [0][0] = 1;
+        matriz [0][1] = 7;
+        matriz [0][2] = 4;
+        matriz [1][0] = 1;
+        matriz [1][1] = 3;
+        matriz [1][2] = 8;
+
         System.out.println(matriz[0].length);
         System.out.println(matriz.length);
     }
