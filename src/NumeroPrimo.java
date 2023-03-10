875fac9a024ca9d55810a7990678c104713c2470
Gaspar Morales <gasparmorales1@gmail.com>
2023-03-10 16:43:37 -0300

    NumeroPrimo

---
+++ src/NumeroPrimo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class NumeroPrimo {
+    private static boolean esNumeroPrimo (int numero){
+        boolean resultado = false;
+        if (numero != 0 && numero != 1 && numero != 4){
+            resultado = true;
+            for (int i = 2; i < numero / 2; i++){
+                if (numero % i == 0){
+                    resultado = false;
+                }
+            }
+        }
+        return resultado;
+    }
+    public static void main(String[] args) {
+        Scanner Teclado = new Scanner(System.in);
+        System.out.print("Ingrese un numero: ");
+        if (esNumeroPrimo(Teclado.nextInt())){
+            System.out.println("es primo");
+        }else {
+            System.out.println("no es primo");
+        }
+    }
+}
